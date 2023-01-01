ec7a9af8b18f58477d0b574a843080fea9ebd4c5
tdjnodj <108401163+tdjnodj@users.noreply.github.com>
2023-01-01 08:58:38 +0800

    fix shadowsocks's share link

--- cold_install.sh
+++ cold_install.sh
@@ -549,7 +549,7 @@ shadowshare() {
 
     echo ""
     yellow "分享链接(可能不兼容shadowsocks-2022): "
-    /etc/shadowsocks-rust/ssurl -e /etc/shadowsocks-rust/ssurl
+    /etc/shadowsocks-rust/ssurl -e /etc/shadowsocks-rust/config.json
     echo "请讲ip地址改成自己的！"
 }
 
